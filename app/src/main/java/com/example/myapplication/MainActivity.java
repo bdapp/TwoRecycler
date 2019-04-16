8fd48b3396b1b38cbac33e1447a77de5a600d3a1
bdapp <belloving@gmail.com>
2019-04-16 18:56:33 +0800

    关联横向滚动的点击事件

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 
 import java.util.ArrayList;
 
@@ -21,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
     private CateAdapter cateAdapter;
     private GoodsAdapter goodsAdapter;
 
-    private int goodsVisiableCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +27,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mContext = MainActivity.this;
 
-
+        // 模拟数据
         for (int i = 1; i <= 15; i++) {
             CateData data = new CateData();
             data.setId(i);
             data.setTitle("分类" + i);
+            if (i == 1) {
+                data.setSelect(true);
+            }
 
-            for (int j = 1; j <= 20; j++) {
+            for (int j = 1; j <= 12; j++) {
                 GoodsData d = new GoodsData();
-                d.setId(i*100 + j);
-                d.setTitle("商品名称" + i*100 + j);
+                d.setId(i * 100 + j);
+                d.setTitle("分类" + i+" - 名称" + i * 100 + j);
                 d.setCate(i);
                 goodsList.add(d);
             }
@@ -54,7 +55,6 @@ public class MainActivity extends AppCompatActivity {
 
         cateAdapter = new CateAdapter(cateList, mHandler);
         _cateRecyclerView.setAdapter(cateAdapter);
-
 
 
         _goodsRecyclerView = findViewById(R.id.goods_recycler_view);
@@ -76,15 +76,10 @@ public class MainActivity extends AppCompatActivity {
                 // 获取最后一个可见item的位置
                 int lastItem = manager.findLastVisibleItemPosition();
 
-                goodsVisiableCount = lastItem - firstItem + 1;
-
-                Log.e(TAG,
-                        "count: " + goodsVisiableCount +", "+goodsList.get(firstItem).getTitle() + ", " + goodsList.get(lastItem).getTitle());
-
                 // 取出第一个item对应的分类id，然后更新分类adapter
                 int id = goodsList.get(firstItem).getCate();
-                for (CateData d: cateList){
-                    if (d.getId() == id){
+                for (CateData d : cateList) {
+                    if (d.getId() == id) {
                         d.setSelect(true);
                     } else {
                         d.setSelect(false);
@@ -92,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 cateAdapter.notifyDataSetChanged();
 
-                _cateRecyclerView.smoothScrollToPosition(id-1);
+                _cateRecyclerView.smoothScrollToPosition(id - 1);
             }
 
             @Override
@@ -103,52 +98,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     ScrollHandler mHandler = new ScrollHandler();
+
     class ScrollHandler extends Handler {
+
+        private int catePosition;
+
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            switch (msg.what){
+            switch (msg.what) {
                 case 1:
-                    int catePosition = (int) msg.obj;
-                    for (int i=0; i<goodsList.size(); i++){
-                        if (goodsList.get(i).getCate() == catePosition){
-//                            _goodsRecyclerView.scrollToPosition(i);
-                            smoothMoveToPosition(_goodsRecyclerView, i);
-                            return;
+                    // 获取需要置顶的大类ID
+                    int position = (int) msg.obj;
+
+                    LinearLayoutManager manager = (LinearLayoutManager) _goodsRecyclerView.getLayoutManager();
+                    int firstItem = manager.findFirstVisibleItemPosition();
+                    int lastItem = manager.findLastVisibleItemPosition();
+
+                    int visibleItemCount = lastItem - firstItem;
+
+                    for (int i = 0; i < goodsList.size(); i++) {
+                        if (goodsList.get(i).getCate() == position) {
+
+                            if (position > catePosition) {
+                                _goodsRecyclerView.scrollToPosition(i + visibleItemCount - 1);
+                            } else {
+                                _goodsRecyclerView.scrollToPosition(i);
+                            }
+                            catePosition = position;
+
+                            break;
                         }
                     }
+
+
+
+
+
                     break;
             }
         }
     }
 
-    /**
-     * 滑动到指定位置
-     *
-     * @param mRecyclerView
-     * @param position
-     */
-    private void smoothMoveToPosition(RecyclerView mRecyclerView, final int position) {
-        // 第一个可见位置
-        int firstItem = mRecyclerView.getChildLayoutPosition(mRecyclerView.getChildAt(0));
-        // 最后一个可见位置
-        int lastItem = mRecyclerView.getChildLayoutPosition(mRecyclerView.getChildAt(mRecyclerView.getChildCount() - 1));
 
-        if (position < firstItem) {
-            // 如果跳转位置在第一个可见位置之前，就smoothScrollToPosition可以直接跳转
-            mRecyclerView.scrollToPosition(position);
-        } else if (position <= lastItem) {
-            // 跳转位置在第一个可见项之后，最后一个可见项之前
-            // smoothScrollToPosition根本不会动，此时调用smoothScrollBy来滑动到指定位置
-            int movePosition = position - firstItem;
-            if (movePosition >= 0 && movePosition < mRecyclerView.getChildCount()) {
-                int top = mRecyclerView.getChildAt(movePosition).getTop();
-                mRecyclerView.scrollBy(0, top);
-            }
-        } else {
-            // 如果要跳转的位置在最后可见项之后，则先调用smoothScrollToPosition将要跳转的位置滚动到可见位置
-            // 再通过onScrollStateChanged控制再次调用smoothMoveToPosition，执行上一个判断中的方法
-            mRecyclerView.scrollToPosition(position);
-        }
-    }
 }
